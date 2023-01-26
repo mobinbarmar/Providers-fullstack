526c66b5fd4b6c38e6d51b74bbfd0bb217273133
Mobin Barmar <barmar.mobin@gmail.com>
2023-01-26 21:34:49 +0330

    bootstrap hbs added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const mainController = require('../controllers/main');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', mainController.home);
 
 module.exports = router;
