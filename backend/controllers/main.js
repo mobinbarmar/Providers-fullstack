1adfa084777c27bf8e590fdb2072c89fe5ea0b70
Mobin Barmar <barmar.mobin@gmail.com>
2023-01-26 21:49:47 +0330

    index controllers and routes added

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -1,3 +1,23 @@
 exports.home = (req, res) => {
-    res.render('index', {title: 'Express'})
+    res.render('index', {title: 'The MEAN Stack Agency'})
+}
+
+exports.about = (req, res) => {
+    res.render('about')
+}
+
+exports.contact = (req, res) => {
+    res.render('contact')
+}
+
+exports.login = (req, res) => {
+    res.render('login')
+}
+
+exports.register = (req, res) => {
+    res.render('register')
+}
+
+exports.forgetpassword = (req, res) => {
+    res.render('forget-password')
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,11 @@ const mainController = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', mainController.home);
+router.get('/about', mainController.about);
+router.get('/contact', mainController.contact);
+router.get('/login', mainController.login);
+router.get('/register', mainController.register);
+router.get('/forget-password', mainController.forgetpassword);
+
 
 module.exports = router;
