a31d354a54bbf6e4e260ecfd7b5a4b26f352de26
Mobin Barmar <barmar.mobin@gmail.com>
2023-02-19 20:23:59 +0330

    provider update added

--- backend/controllers/providers.js
+++ backend/controllers/providers.js
@@ -16,3 +16,36 @@ exports.details = (req, res) => {
         company: provider.company
     })
 }
+// Edit Form
+exports.edit = (req, res) => {
+    let id = req.params.id
+    let provider = providers.find(provider => provider.id == id)
+    res.render('providers/providers-edit', {
+        id,
+        title: 'Edit',
+        provider
+    })
+}
+// Update Form
+exports.update = (req, res) => {
+    let id = req.params.id
+    let provider = providers.find(provider => provider.id == id)
+    provider.firstname = req.body.firstname
+    provider.lastname = req.body.lastname
+    provider.position = req.body.position
+    provider.company.company_name = req.body.company.company_name
+    provider.company.address = req.body.company.address
+    provider.company.address2 = req.body.company.address2
+    provider.company.city = req.body.company.city
+    provider.company.state = req.body.company.state
+    provider.company.postal_code = req.body.company.
+    provider.company.phone = req.body.company.phone
+    provider.company.email = req.body.company.email
+    provider.company.description = req.body.company.description
+    provider.company.tagline = req.body.company.tagline
+
+
+    res.render('providers/providers-update', {
+        title: 'Update',
+    })
+}
--- backend/routes/providers.js
+++ backend/routes/providers.js
@@ -7,6 +7,10 @@ const providersController = require('../controllers/providers');
 router.get('/', providersController.list);
 // GET details page
 router.get('/details/:id', providersController.details);
+// GET edit page
+router.get('/edit/:id', providersController.edit);
+// GET post page
+router.post('/update/:id', providersController.update);
 
 
 
