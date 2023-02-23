8602f942953555c7026a517c2db22c66e416cb8e
Mobin Barmar <barmar.mobin@gmail.com>
2023-02-24 00:56:19 +0330

    reactive forms added with some bugs

--- frontend/mean-stack-agency/src/app/models/providers.data.ts
+++ frontend/mean-stack-agency/src/app/models/providers.data.ts
@@ -1,4 +1,4 @@
-export let provider = [
+export let providers = [
     {
         "id": 414532,
         "firstname": "Marylinda",
--- frontend/mean-stack-agency/src/app/providers/add-providers/add-providers.component.ts
+++ frontend/mean-stack-agency/src/app/providers/add-providers/add-providers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ProviderClass } from 'src/app/models/providers.class';
+import { providers } from 'src/app/models/providers.data';
 
 @Component({
   selector: 'app-add-providers',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProvidersComponent implements OnInit {
 
+  submitted = false
+  provider = new ProviderClass()
+  providersForm!: FormGroup
+
   constructor() { }
 
   ngOnInit(): void {
+    this.providersForm = new FormGroup({
+      firstname: new FormControl(),
+      lastname: new FormControl(),
+      position: new FormControl(),
+      email: new FormControl(),
+      phone: new FormControl(),
+      company_name: new FormControl(),
+      address: new FormControl(),
+      address2: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      postal_code: new FormControl(),
+      description: new FormControl(),
+      tagline: new FormControl(),
+
+    })
   }
 
+  handleSubmit() {
+    console.log(this.providersForm.value);
+
+    let newId: number
+    while (true) {
+      newId = Math.floor(Math.random() * 10000) + 99999
+      if (providers.findIndex(el => el.id == newId) == -1) {
+        break
+      }
+    }
+
+
+    let p = this.providersForm.value
+    this.provider.id = newId
+    this.provider.firstname = p.firstname
+    this.provider.lastname = p.lastname
+    this.provider.position = p.position
+    this.provider.company = {
+      company_name: p.company_name,
+      address: p.address,
+      address2: p.address2,
+      city: p.city,
+      state: p.state,
+      postal_code: p.postal_code,
+      phone: p.phone,
+      email: p.email,
+      description: p.description,
+      tabline: p.tabline
+    }
+    providers.push(this.provider)
+    this.submitted = true
+
+  }
 }
