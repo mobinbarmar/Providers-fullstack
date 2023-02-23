7ef6b8edb3f02007cc8f3d625143c347ed09bce8
Mobin Barmar <barmar.mobin@gmail.com>
2023-02-24 00:11:07 +0330

    css bugs fixed

--- frontend/mean-stack-agency/src/app/about/about.module.ts
+++ frontend/mean-stack-agency/src/app/about/about.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { AboutComponent } from './about.component';
     AboutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [AboutComponent],
   providers: []
